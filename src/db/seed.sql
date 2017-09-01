2d3c2d32f4b9f9c0854cdc55671935079503a1b6
Ryan Kent <imsolost@gmail.com>
2017-09-01 13:06:15 -0700

    updated review seed data

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -6,3 +6,19 @@ VALUES
   ('Melodrama', 'Lorde'),
   ('In Rainbows', 'Radiohead')
 ;
+
+INSERT INTO
+  users (username, email, password)
+VALUES
+  ('a', 'a@a.com', 'a'),
+  ('b', 'b@b.com', 'b')
+;
+
+INSERT INTO
+  reviews (user_id, album_id, content)
+VALUES
+  (1, 1, 'first album review'),
+  (1, 2, 'second album review'),
+  (1, 3, 'third album review'),
+  (2, 1, 'first album review')
+;
